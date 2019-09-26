f317dbd4c47d43c988d663590f3f10ba78c2d7b5
Minh Le Ngoc <minhle@Minhs-iMac.local>
2019-09-26 20:54:25 +0700

    layout

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,19 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/essential', '@vue/standard'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-function-paren': 0,
+    'comma-dangle': 0,
+    semi: 0,
+    quotes: 0,
+    'no-unused-vars': 0,
+    'no-console': 0,
+    'no-debugger': 1
   },
   parserOptions: {
     parser: 'babel-eslint'
   }
-}
+};
--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,49 @@
+
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar clipped-left app color="amber darken-2" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Music Search Tool</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer clipped v-model="drawer" app>
+      <v-list dense>
+        <v-list-item :to="item.to" v-for="(item, index) in menu" :key="index">
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.name }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+export default {
+  name: "app",
+  data: () => ({
+    drawer: null,
+    menu: [
+      {
+        name: "Tracks",
+        icon: "mdi-music",
+        to: "/"
+      },
+      {
+        name: "Artists",
+        icon: "mdi-person",
+        to: "/about"
+      }
+    ]
+  })
+};
+</script>
