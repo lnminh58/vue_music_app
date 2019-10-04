bf770f50623e8a7b18aeddccd0a269f054c88195
Phước Thiệm <duongphuocthiem@gmail.com>
2019-10-04 17:11:28 +0700

    Passing TrackID to TrackDetailPage

---
+++ src/views/TractDetail.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <button class="btn btn-primary" @click="getTrackID">Show TrackID</button>
+    <p>Track ID: {{ track.trackID }}</p>
+  </div>
+</template>
+
+<script>
+import { eventBus } from "../main";
+
+export default {
+  data() {
+    return {
+      track: {
+        trackID: ''
+      }
+    };
+  },
+  created() {
+    eventBus.$on("getTrackID", (TrackID) => {
+      this.track.trackID = TrackID;
+    })
+  },
+  methods: {
+    getTrackID() {
+      console.log('detail ID', this.track.trackID);
+    }
+  }
+};
+</script>
